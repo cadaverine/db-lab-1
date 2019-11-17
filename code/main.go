ecf3e1b15c1315add60b23e1c3d7cfb251e3e908
cadaverine <kennville@gmail.com>
2019-11-17 22:26:31 +0300

    feat(main.go): add writing to 'institution.csv'

--- code/main.go
+++ code/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+func writeToCSV(path string, values [][]string) {
+	file, err := os.Create("../test-data/institutions.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	writer.WriteAll(values)
+	writer.Flush()
+}
 
 func main() {
-	fmt.Println("")
+	var institutionsNum int
+
+	fmt.Print("Type institutions num: ")
+	fmt.Scanln(&institutionsNum)
+
+	institutionsData := generateInstitutionsData(institutionsNum)
+
+	writeToCSV("../test-data/institutions.csv", institutionsData)
 }
