3d006ea8c5b0bfa7284d63b21801c9cbf1f02bc0
cadaverine <kennville@gmail.com>
2019-11-20 00:18:33 +0300

    feat(main.go): add students data generation

--- cmd/db-lab-1/main.go
+++ cmd/db-lab-1/main.go
@@ -21,12 +21,17 @@ func writeToCSV(path string, values [][]string) {
 }
 
 func main() {
-	var institutionsNum int
+	var institutionsNum, studentsNum int
 
 	fmt.Print("Type institutions num: ")
 	fmt.Scanln(&institutionsNum)
 
 	institutionsData := g.GenerateInstitutionsData(institutionsNum)
-
 	writeToCSV("test-data/institutions.csv", institutionsData)
+
+	fmt.Print("Type students num: ")
+	fmt.Scanln(&studentsNum)
+
+	studentsData := g.GenerateStudentsData(studentsNum)
+	writeToCSV("test-data/students.csv", studentsData)
 }
