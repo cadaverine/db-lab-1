fdf8c8f1f06deed54981d8248c0d3a0326a12303
cadaverine <kennville@gmail.com>
2019-11-20 22:44:47 +0300

    feat(teachers_generator.go): create 'GenerateTeachersData'

--- db-generators/teachers_generator.go
+++ db-generators/teachers_generator.go
@@ -1,1 +1,20 @@
 package dbgenerators
+
+// GenerateTeachersData - generate test data for table 'students'
+func GenerateTeachersData(rowsNum int) [][]string {
+	result := make([][]string, rowsNum+1)
+
+	result[0] = []string{"first_name", "last_name", "phone", "email", "academic_degree"}
+
+	for i := 1; i <= rowsNum; i++ {
+		firstName, _ := getRandomValue(firstNames)
+		lastName, _ := getRandomValue(lastNames)
+		phone := generatePhone("89", 11)
+		email := generateEmail(firstName, lastName)
+		degree, _ := getRandomValue(academicDegrees)
+
+		result[i] = []string{firstName, lastName, phone, email, degree}
+	}
+
+	return result
+}
